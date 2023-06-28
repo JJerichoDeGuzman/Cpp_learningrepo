c3235b2008367dbc11ccb71c1057e9bee8d6c8e3
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-06-28 17:43:03 +0800

    -

--- .vscode/TEST.cpp
+++ .vscode/TEST.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 
-int main () {
-using std::cout;
-if (true)  { cout << "yes\n"; }  // yes
-if (false) { cout << "yes\n"; }  // –
-if (23)    { cout << "yes\n"; }  // yes (23 - true)
-if (0)     { cout << "yes\n"; }  // – (0 - false)
-int i = 0;
-cout << "enter an integer: ";
-std::cin >> i;
-if (i < 0) { 
-  cout << "negative\n"; 
-} else if (i == 0) {
-  cout << "zero\n"; 
-} else {
-  cout << "positive\n"; 
-}
-}
+int main() {
+    int myInteger; // default-initialized to an unspecified value
+    float myFloat; // default-initialized to an unspecified value
+    bool myBool; // default-initialized to false
+    char myChar; // default-initialized to '\0'
+    
+    std::cout << "myInteger: " << myInteger << std::endl;
+    std::cout << "myFloat: " << myFloat << std::endl;
+    std::cout << "myBool: " << std::boolalpha << myBool << std::endl;
+    std::cout << "myChar: " << myChar << std::endl;
 
-
+    return 0;
+}
