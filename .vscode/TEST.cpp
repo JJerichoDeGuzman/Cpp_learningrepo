f3635d108e07f763a52eef6ff8450324cd2ced32
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-06-28 18:33:20 +0800

    .

--- .vscode/TEST.cpp
+++ .vscode/TEST.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 
-int main() {
-    int myInteger; // default-initialized to an unspecified value
-    float myFloat; // default-initialized to an unspecified value
-    bool myBool; // default-initialized to false
-    char myChar; // default-initialized to '\0'
-    
-    std::cout << "myInteger: " << myInteger << std::endl;
-    std::cout << "myFloat: " << myFloat << std::endl;
-    std::cout << "myBool: " << std::boolalpha << myBool << std::endl;
-    std::cout << "myChar: " << myChar << std::endl;
+int main()
+{
+    std::cout << "Hi!" << std::endl;
+    std:: cout << "My name is";
 
-    return 0;
+return 0;
 }
