f151c0d385ded3dbfd31fde9dc9551268d341aea
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-01 15:48:11 +0800

    Function call test

---
+++ Chapter_2_CPP/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // for std::cout
+
+// Definition of user-defined function doPrint()
+void doPrint() // doPrint() is the called function in this example
+{
+    std::cout << "In doPrint()\n";
+}
+
