d773638cb6adf31bdde3c3220788f7b491368ae0
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-02 13:02:39 +0800

    Chapter 2 exercises

--- Chapter_2_CPP/Chp2-functions_and_files.cpp
+++ Chapter_2_CPP/Chp2-functions_and_files.cpp
@@ -1,44 +1,16 @@
 #include <iostream>
 
-void CircArea_Calc(double circ_radius) {    
-    std::cout << 3.14159265 * (circ_radius * circ_radius); << std::endl; 
+void reverseString(std::string& str) {
+   char ch;
+   for (int index = 0, len = str.length(); index < len/2; index++) {
+      ch = str[index];
+      str[index] = str[len-1-index];
+      str[len-1-index] = ch;
+   }
 }
 
-void RectArea_Calc (double baseR, double HeightR) {
-    std::cout << " The area of rectangle is " << BaseR * HeightR << std::endl; 
-}
-
-void TriaArea_Calc (double baseT, double HeighT) {
-     std::cout << " The area of triangle is " << (BaseT * HeightT) / 2 << std::endl;
-}
-
-
-int main () {
-    std::cout << " Choose shape: Circle or Rectangle, Triangle: " << std::endl;
-    std::string shape_choice {};
-    std::cin >> shape_choice;
-
-    if { (shape_choice == "triangle")
-
-    std::cout << " What is the radius of your triangle " << std::endl;
-    double radius_input {};
-    std::cin >> radius_input;
-    CircArea_Calc(radius_input)
-    
-    }
-
-    else {
-
-    std::cout << " Enter the base and height, respectively " << std::endl;
-    double base_input {};
-    double height_input {};
-
-    std::cin >> base_input >> height_input;
-    RectArea_Calc(base_input, height_input)
-    TriaArea_Calc(base_input, height_input)
-
-    }
-   
-
-    return 0;
+int main() {
+   std::string str = "Hello, World!";
+   reverseString(str);
+   std::cout << str << std::endl; // Outputs: "!dlroW ,olleH"
 }
