faa940bb24468e37ee714375c2fb2e3e945c14f1
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-02 12:50:17 +0800

    Prboelm 1 solved

--- Chapter_2_CPP/Chp2-functions_and_files.cpp
+++ Chapter_2_CPP/Chp2-functions_and_files.cpp
@@ -1,16 +1,44 @@
 #include <iostream>
 
-void double_number(int num)
-{
-    std::cout << " The double value of " << num << " is " << num * num <<std::endl; 
-
+void CircArea_Calc(double circ_radius) {    
+    std::cout << 3.14159265 * (circ_radius * circ_radius); << std::endl; 
 }
+
+void RectArea_Calc (double baseR, double HeightR) {
+    std::cout << " The area of rectangle is " << BaseR * HeightR << std::endl; 
+}
+
+void TriaArea_Calc (double baseT, double HeighT) {
+     std::cout << " The area of triangle is " << (BaseT * HeightT) / 2 << std::endl;
+}
+
 
 int main () {
+    std::cout << " Choose shape: Circle or Rectangle, Triangle: " << std::endl;
+    std::string shape_choice {};
+    std::cin >> shape_choice;
 
-    int double_number(5);
+    if { (shape_choice == "triangle")
 
-    return 0; 
+    std::cout << " What is the radius of your triangle " << std::endl;
+    double radius_input {};
+    std::cin >> radius_input;
+    CircArea_Calc(radius_input)
+    
+    }
 
+    else {
+
+    std::cout << " Enter the base and height, respectively " << std::endl;
+    double base_input {};
+    double height_input {};
+
+    std::cin >> base_input >> height_input;
+    RectArea_Calc(base_input, height_input)
+    TriaArea_Calc(base_input, height_input)
+
+    }
+   
+
+    return 0;
 }
-
