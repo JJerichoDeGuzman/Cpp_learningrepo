e69b8a29769a793af637bac3a03ed49efc426673
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-04 11:45:13 +0800

    Update Chp2-functions_and_files.cpp

--- Chapter_2_CPP/Chp2-functions_and_files.cpp
+++ Chapter_2_CPP/Chp2-functions_and_files.cpp
@@ -1,16 +1,30 @@
-#include <iostream> 
-using namespace std;
+#include <iostream>
+#include <cmath>
 
-string reverseString(const string& str) {
-   string rev(str.length(), ' ');
-   for (int index = 0, len = str.length(); index < len; index++) {
-      rev[index] = str[len-1-index];
-   }
-   return rev;
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void prime_func(int prime_num) {
+    if (isPrime(prime_num)) {
+        std::cout << prime_num << " is a prime number" << std::endl;
+    }
+    else {
+        std::cout << prime_num << " is not a prime number" << std::endl;
+    }
 }
 
 int main() {
-   string str = "Hello, World!";
-   string rev = reverseString(str);
-   cout << rev << endl; // Outputs: "!dlroW ,olleH"
+    std::cout << "Enter an integer: " << std::endl;
+    int prime_checker;
+    std::cin >> prime_checker;
+    prime_func(prime_checker);
+
+    return 0;
 }
