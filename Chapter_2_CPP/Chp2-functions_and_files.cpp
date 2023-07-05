4c9ac069beb570573d284a63303e36fbd2356ecc
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-05 18:37:02 +0800

    .

--- Chapter_2_CPP/Chp2-functions_and_files.cpp
+++ Chapter_2_CPP/Chp2-functions_and_files.cpp
@@ -1,63 +1,9 @@
 #include <iostream>
 
-bool isFibonacci(int num) {
-    int a = 0;
-    int b = 1;
-
-    if (num == a || num == b) {
-        return true;
-    }
-
-    while (b <= num) {
-        int temp = b;
-        b = a + b;
-        a = temp;
-
-        if (b == num) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-void generateFibonacci(int length) {
-    int first = 0;
-    int second = 1;
-
-    std::cout << "Fibonacci Sequence: ";
-
-    // Print the first two numbers
-    std::cout << first << " " << second << " ";
-
-    for (int i = 2; i < length; i++) {
-        int next = first + second;
-        std::cout << next << " ";
-
-        // Update the values for the next iteration
-        first = second;
-        second = next;
-    }
-
-    std::cout << std::endl;
-}
-
-int main() {
-    int length;
-    std::cout << "Enter the length of the Fibonacci sequence: ";
-    std::cin >> length;
-
-    generateFibonacci(length);
-
-    int input;
-    std::cout << "Enter a number to check if it's in the Fibonacci sequence: ";
-    std::cin >> input;
-
-    if (isFibonacci(input)) {
-        std::cout << input << " is in the Fibonacci sequence" << std::endl;
-    } else {
-        std::cout << input << " is not in the Fibonacci sequence" << std::endl;
-    }
-
+int main()
+{
+    int a { 10 };
+    int b { 0 };
+    std::cout << a << " / " << b << " = " << a / b << '\n'; // division by 0 is undefined in mathematics
     return 0;
 }
--- Chapter_2_CPP/Chp2_probs_solns__compilation.md
+++ Chapter_2_CPP/Chp2_probs_solns__compilation.md
@@ -151,6 +151,73 @@ int main() {
 
 **Solution** <hr>
 
+```
+#include <iostream>
+
+bool isFibonacci(int num) {
+    int a = 0;
+    int b = 1;
+
+    if (num == a || num == b) {
+        return true;
+    }
+
+    while (b <= num) {
+        int temp = b;
+        b = a + b;
+        a = temp;
+
+        if (b == num) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void generateFibonacci(int length) {
+    int first = 0;
+    int second = 1;
+
+    std::cout << "Fibonacci Sequence: ";
+
+    // Print the first two numbers
+    std::cout << first << " " << second << " ";
+
+    for (int i = 2; i < length; i++) {
+        int next = first + second;
+        std::cout << next << " ";
+
+        // Update the values for the next iteration
+        first = second;
+        second = next;
+    }
+
+    std::cout << std::endl;
+}
+
+int main() {
+    int length;
+    std::cout << "Enter the length of the Fibonacci sequence: ";
+    std::cin >> length;
+
+    generateFibonacci(length);
+
+    int input;
+    std::cout << "Enter a number to check if it's in the Fibonacci sequence: ";
+    std::cin >> input;
+
+    if (isFibonacci(input)) {
+        std::cout << input << " is in the Fibonacci sequence" << std::endl;
+    } else {
+        std::cout << input << " is not in the Fibonacci sequence" << std::endl;
+    }
+
+    return 0;
+}
+
+
+```
 
 ##### Exercise 5 - Factorial
 **Problem** <hr>
