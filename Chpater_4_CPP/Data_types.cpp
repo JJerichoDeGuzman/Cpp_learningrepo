2497642a0f2f07debd389f22fa0c3d06de141120
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-07 14:00:14 +0800

    pointers

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -1,38 +1,17 @@
 #include <iostream> 
 
-void Celsius_converter(double Celc)
-{
-    std::cout << Celc << " is " << ((Celc  * 9) / 5 ) + 32 << " Farenheit " << std::endl;
-}
+int main() {
 
-void Farenheit_converter(double Faren) {
+    std::cout << " Write a single character " << std::endl;
+    char Holder{}; 
+    std::cin >> Holder; 
 
-    std::cout << Faren<< " is " << ((Faren  - 32) *( 5 ) / 9 ) << " Celsius " << std::endl;
+    int NewHolder = static_cast<int>(Holder);
 
-    }
-int main () {
-   
-    std::cout << " Type C to convert Celsius to Farenheit "  << std::endl; 
-    std::cout << " Type F to cover Farenheit to celsius " << std::endl; 
-    std::string user_input {};
-    std::cin >> user_input; 
+    std:: cout << NewHolder  << std::endl; 
 
-    if (user_input == "C") {
 
-        std::cout << " Enter  Celsius "  <<  std::endl; 
-        double Celcius_Input {};
-        std::cin >> Celcius_Input;
-        Celsius_converter(Celcius_Input);
-    
-    }
-    
-    else if (user_input == "F") {
-        std::cout << " Enter  Farenheit "  <<  std::endl; 
-        double Faren_Input {};
-        std::cin >> Faren_Input;
-        Farenheit_converter(Faren_Input); 
-    }
-    
 
-    
+
+    return 0; 
 }
