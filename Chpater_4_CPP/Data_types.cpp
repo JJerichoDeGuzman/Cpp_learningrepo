cfc396218b88eb1825bedcd2504150c2dcdd001b
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-08 18:18:22 +0800

    flaot

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     std::cout << std::setprecision(16); // show 16 digits of precision
-    std::cout << 3.33333333333333333333333333333333333333f <<'\n'; // f suffix means float
+    std::cout << 3.33333333333333333333333333333333333333ld <<'\n'; // f suffix means float
     std::cout << 3.33333333333333333333333333333333333333 << '\n'; // no suffix means double
 
     return 0;
