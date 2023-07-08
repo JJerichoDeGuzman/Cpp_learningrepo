77a63448dea11c6b1c2c7cecb2d0f86e370e0c3c
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-08 18:17:13 +0800

    floats

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <iomanip> // for output manipulator std::setprecision()
 
 int main()
 {
-    std::cout << 8 / 5 << '\n';
+    std::cout << std::setprecision(16); // show 16 digits of precision
+    std::cout << 3.33333333333333333333333333333333333333f <<'\n'; // f suffix means float
+    std::cout << 3.33333333333333333333333333333333333333 << '\n'; // no suffix means double
+
     return 0;
 }
