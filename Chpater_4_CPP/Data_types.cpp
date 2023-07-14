13b5e337ffc6d12f3a6edbcf8626cfd9d4b453fc
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-14 21:43:51 +0800

    .

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <string> // For std::string and std::getline
 
 int main()
 {
-    std::cout << 5 << '\n';  // 5 (no suffix) is type int (by default)
-    std::cout << 5L << '\n'; // 5L is type long
-    std::cout << 5u << '\n'; // 5u is type unsigned int
+    std::cout << "Enter your full name: ";
+    std::string name{};
+    std::getline(std::cin >> std::ws, name); // read a full line of text into name
+
+    std::cout << "Enter your favorite color: ";
+    std::string color{};
+    std::getline(std::cin >> std::ws , color); // read a full line of text into color
+
+    std::cout << "Your name is " << name << " and your favorite color is " << color << '\n';
 
     return 0;
 }
