1e87c9441f7b66b2852b66f08cd9934d3121edea
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-14 23:07:16 +0800

    .

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-#include <string> // For std::string and std::getline
+#include <string>
 
-int main()
-{
-    std::cout << "Enter your full name: ";
-    std::string name{};
-    std::getline(std::cin >> std::ws, name); // read a full line of text into name
+int main() {
 
-    std::cout << "Enter your favorite color: ";
-    std::string color{};
-    std::getline(std::cin >> std::ws , color); // read a full line of text into color
+    std::cout << " Enter your full name" <<std::endl; 
+    std::string fullName {};
+    std.getline(std::cin >> std::ws, fullName); 
 
-    std::cout << "Your name is " << name << " and your favorite color is " << color << '\n';
+    
 
-    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;     
 }
