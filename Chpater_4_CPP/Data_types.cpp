ce8d6a8786574d1cc2a6be7cb9f289c56d658483
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-14 23:18:24 +0800

    uploaded markdown

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -5,23 +5,16 @@ int main() {
 
     std::cout << " Enter your full name" <<std::endl; 
     std::string fullName {};
-    std.getline(std::cin >> std::ws, fullName); 
+    std::getline(std::cin >> std::ws, fullName); 
 
-    
+    std::cout << " How old are you: " << std::endl;
+    int age {}; 
+    std::cin >> age; 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // age is signed, and name.length() is unsigned -- we shouldn't mix these
+    // We'll convert name.length() to a signed value
+    int letters {static_cast<int>(fullName.length())}; 
+    std::cout << "Your age + length of your name is: " << age + letters  << '\n'; 
 
 
 return 0;     
