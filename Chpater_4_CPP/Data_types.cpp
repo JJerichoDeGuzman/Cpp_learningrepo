5ea6081c7667e14e12019e219a900970f7514c55
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-15 19:05:16 +0800

    ..

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-#include <string>
+#include <string_view>
 
-int main() {
+// str provides read-only access to whatever argument is passed in
+void printSV(std::string_view str) // now a std::string_view
+{
+    std::cout << str << '\n';
+}
 
-    std::cout << " Enter your full name" <<std::endl; 
-    std::string fullName {};
-    std::getline(std::cin >> std::ws, fullName); 
+int main()
+{
+    std::string_view s{ "Hello, world!" }; // now a std::string_view
+    printSV(s);
 
-    std::cout << " How old are you: " << std::endl;
-    int age {}; 
-    std::cin >> age; 
-
-    // age is signed, and name.length() is unsigned -- we shouldn't mix these
-    // We'll convert name.length() to a signed value
-    int letters {static_cast<int>(fullName.length())}; 
-    std::cout << "Your age + length of your name is: " << age + letters  << '\n'; 
-
-
-return 0;     
+    return 0;
 }
