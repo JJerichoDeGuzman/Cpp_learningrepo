acc8af73a80f8e682212670c7af4c11ab9fac1c2
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-16 12:26:29 +0800

    std::string_view initialization

--- Chpater_4_CPP/Data_types.cpp
+++ Chpater_4_CPP/Data_types.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <string>
 #include <string_view>
-
-// str provides read-only access to whatever argument is passed in
-void printSV(std::string_view str) // now a std::string_view
-{
-    std::cout << str << '\n';
-}
 
 int main()
 {
-    std::string s{ "Hello, world!" }; // now a std::string_view
-    printSV(s);
+    std::string_view s1 { "Hello, world!" }; // initialize with C-style string literal
+    std::cout << s1 << '\n';
+
+    std::string s{ "Hello, world!" };
+    std::string_view s2 { s };  // initialize with std::string
+    std::cout << s2 << '\n';
+
+    std::string_view s3 { s2 }; // initialize with std::string_view
+    std::cout << s3 << '\n';
 
     return 0;
 }
