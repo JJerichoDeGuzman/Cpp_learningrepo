920cf634f9b1c9f2733e9674ea1fc50e81785e4e
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-17 14:52:46 +0800

    added markdown for chp5

--- Chapter_5_CPP/Operators.cpp
+++ Chapter_5_CPP/Operators.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
+    //Check if input is odd or even
+    void isEven(int inputCheck){
+        int checker {inputCheck % 2};
+
+        if (checker == 0){
+            std::cout << inputCheck << " is even. \n";
+        }
+        else {
+            std::cout << inputCheck << " is odd. \n";
+        }
+
+    }
+
+
 int main(){
     
-    std::cout << 26 % 3; 
+    //Ask for the user 
+    std::cout << "Enter the integer: \n";
+    int userIntegerInput {};
+    std::cin >> userIntegerInput;
 
-
+    isEven(userIntegerInput);
 
     return 0;
 }
