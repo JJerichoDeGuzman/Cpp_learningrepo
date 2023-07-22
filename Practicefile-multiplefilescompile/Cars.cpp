79d4502f4b575ed902865b347d025326715be240
JJerichoDeGuzman <125799581+JJerichoDeGuzman@users.noreply.github.com>
2023-07-22 20:53:57 +0800

    Compiling multiple files

---
+++ Practicefile-multiplefilescompile/Cars.cpp
@@ -0,0 +1,17 @@
+#include "Cars.h"
+
+using namespace std;
+
+Cars::cars()
+{
+
+}
+Cars::~cars()
+{
+
+
+}
+void Cars::printout()
+{
+    cout << "Cars Rock!!" << endl; 
+}
---
+++ Practicefile-multiplefilescompile/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Cars/Cars/h"
+
+int main()
+{
+    Cars c; 
+    cout << "Hello, World!" << endl;
+    c.printout();
+
+
+
+}
